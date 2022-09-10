acf580ecb4e77d15b35eba663c2c88057c8e90f3
watame <pandora.border@gmail.com>
2022-09-10 21:19:31 +0900

    :+1: script.jsを作成し、必要そうな要素とやることを列挙

---
+++ script.js
@@ -0,0 +1,22 @@
+// TODO
+// 取得したい要素の把握
+// 課題編集
+//   1. 状態のクラス: class="ticket__properties-item -status"
+//   2-1. 予定時間: id="estimatedHours"
+//   2-2. 実績時間: id="actualHours"
+//   3. 「保存」ボタン: class="button button--primary -w-fixed-small"
+// コメント追加
+//   1. 状態のクラス: class="comment-editor__change-statuses-status"
+//   2. 余実管理: class="comment-editor__change-statuses-status"
+//      - 予定時間: class="numeric input-text -w-xx-small"
+//      - 実績時間: class="numeric input-text -w-xx-small _mg-l-5"
+//   3. 「登録する」ボタン: class="button button--primary -w-fixed-small"
+
+// TODO: URLでの場合わけ
+// 課題編集: https://test.backlog.com/view/TEST-1/edit
+// コメント編集: https://test.backlog.com/board/TEST?selectedIssueKey=TEST-1
+
+// TODO: 各エレメントの取得
+
+// TODO: 余実管理の場合わけ
+// ステータスによって余実を入れたり、入れなかったり。。。
